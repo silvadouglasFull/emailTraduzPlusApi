708ba55c9684f8d4ca3089c8cd978692f65037c4
silvadouglasFull <suportedouglaspostopratico@gmail.com>
2025-07-04 13:54:17 -0300

    atualizado o valor de Access-Control-Allow-Headers

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -20,7 +20,7 @@ class CorsMiddleware
             'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
             'Access-Control-Allow-Credentials' => 'true',
             'Access-Control-Max-Age'           => '86400',
-            'Access-Control-Allow-Headers'     => 'Content-Type, API-KEY, X-Requested-With'
+            'Access-Control-Allow-Headers'     => 'Content-Type, Api-Key, X-Requested-With'
         ];
 
         if ($request->isMethod('OPTIONS')) {
